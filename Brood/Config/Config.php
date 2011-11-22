40d61192d40042002938ee0d7f87c6e53f365898
Jonathan Tai <jtai@ign.com>
2011-11-21 20:37:27 -0800

    remove require_once calls

--- Brood/Config/Config.php
+++ Brood/Config/Config.php
@@ -31,8 +31,6 @@ class Config
 
     public function getHostGroups()
     {
-        require_once __DIR__ . '/HostGroup.php';
-
         $hostgroups = array();
         foreach ($this->xml->hostgroup as $hostgroup) {
             $hostgroup = new HostGroup($hostgroup);
@@ -43,8 +41,6 @@ class Config
 
     public function getActions()
     {
-        require_once __DIR__ . '/Action.php';
-
         $actions = array();
         foreach ($this->xml->action as $action) {
             $actions[] = new Action($action);
