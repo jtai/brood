7b756c17ec161cf10a182e35bb5dc09e7342bc3d
Jonathan Tai <jtai@ign.com>
2011-11-29 14:25:24 -0800

    add email announcement action

---
+++ library/Brood/Action/Announce/Email.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Brood\Action\Announce;
+
+use Brood\Action\AbstractAction,
+    Brood\Log\Logger;
+
+class Email extends AbstractAction
+{
+    public function execute()
+    {
+        $from    = (string) $this->getRequiredParameter('from');
+        $subject = (string) $this->getRequiredParameter('subject');
+
+        $message = sprintf(
+            "User: %s\n\n%s\n\nChanged Files:\n%s",
+            $this->getParameter('user'),
+            $this->getParameter('description'),
+            $this->getParameter('changelog')
+        );
+
+        $diffUrl = (string) $this->getParameter('diff_url');
+        if (!empty($diffUrl)) {
+            $message .= sprintf(
+                "\n\n" . $diffUrl,
+                $this->getParameter('from_revision'),
+                $this->getParameter('to_revision')
+            );
+        }
+
+        $headers = join("\r\n", array(
+            'From: ' . $from,
+        ));
+
+        $envelopeSender = preg_match('/<(.+)>/', $from, $regs) ? $regs[1] : $from;
+        $params = '-f' . $envelopeSender;
+
+        foreach ($this->getRequiredParameter('to') as $to) {
+            $this->log(Logger::INFO, __CLASS__, sprintf('Sending notification email to %s', $to));
+
+            mail($to, $subject, $message, $headers, $params);
+        }
+    }
+}
