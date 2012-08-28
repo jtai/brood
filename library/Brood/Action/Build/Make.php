7117fc4f7de91d3828b710c5bf8f73176f941da5
Nicholas Calugar <njcalugar@gmail.com>
2012-08-28 10:17:58 -0700

    Initial commit of the Make action
    Takes a space-separated list of targets that should be passed to make.

---
+++ library/Brood/Action/Build/Make.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Brood
+ *
+ * @category   Brood
+ * @package    Brood_Action
+ * @copyright  Copyright (c) 2011 IGN Entertainment, Inc. (http://corp.ign.com/)
+ * @license    http://www.opensource.org/licenses/mit-license.php     MIT License
+ */
+
+namespace Brood\Action\Build;
+
+use Brood\Action\AbstractAction,
+    Brood\Log\Logger;
+
+/**
+ * Run make with specified targets.
+ *
+ * The targets configuration should simply be a space-separated
+ * list of the targets that should be passed to make.
+ *
+ * @category   Brood
+ * @package    Brood_Action
+ * @copyright  Copyright (c) 2011 IGN Entertainment, Inc. (http://corp.ign.com/)
+ * @license    http://www.opensource.org/licenses/mit-license.php     MIT License
+ */
+class Make extends AbstractAction
+{
+    public function execute()
+    {
+        $this->chdir($this->getRequiredParameter('directory'));
+
+        $targets = $this->getParameter('targets');
+
+        // Run make with configured targets
+        $command = 'make' . empty($targets) ? '' : ' ' . $targets;
+        unset($output);
+
+        $this->log(Logger::INFO, __CLASS__, 'Performing ' . $command);
+
+        $this->sudo($command, $output, $return_var, (string) $this->getParameter('sudo'));
+    }
+}
